33a2467de43f5dc593941c25dd8d264aa72d5789
John Galt <johngaltfirstrun@gmail.com>
2025-12-05 22:11:57 -0500

    common: cleanup blur flag
    one prop is deprecated

--- config/common.mk
+++ config/common.mk
@@ -171,8 +171,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 # Blur
 ifeq ($(TARGET_ENABLE_BLUR), true)
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1 \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1
 else
 PRODUCT_PRODUCT_PROPERTIES += \
