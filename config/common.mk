4766043b0bdf30e76e648df29339fd1ef50da6bf
John Galt <johngaltfirstrun@gmail.com>
2026-01-15 14:45:34 -0500

    Revert "config: Switch to glif_expressive SUW theme"
    This reverts commit b2779d0a2295f345ecce4cfd825a9d803e262671.
    
    Has issues on vanilla

--- config/common.mk
+++ config/common.mk
@@ -202,10 +202,6 @@ ifeq ($(TARGET_BUILD_LINDROID),true)
     $(call inherit-product, vendor/lindroid/lindroid.mk)
 endif
 
-# SetupWizard theme
-PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.theme=glif_expressive
-
 #OTA tools
 PRODUCT_HOST_PACKAGES += \
     signapk \
